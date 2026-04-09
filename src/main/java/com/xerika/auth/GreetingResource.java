af39a52b93066b58d6753ad54416ab8f1eef83c0
winterheatherica <winterheatherica@gmail.com>
2026-04-10 03:14:13 +0700

    init

---
+++ src/main/java/com/xerika/auth/GreetingResource.java
@@ -0,0 +1,7 @@
+package com.xerika.auth;
+
+public class GreetingResource {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/xerika/auth/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.xerika.auth;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
